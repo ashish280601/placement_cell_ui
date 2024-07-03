1c872c02573376de580bb79d88fbad726053c08a
Ashish Mehra <119057692+ashish280601@users.noreply.github.com>
2024-07-03 06:16:27 +0000

    added interview slice in store

--- store.jsx
+++ store.jsx
@@ -3,13 +3,15 @@ import authSlice from "./src/slice/authSlice";
 import studentSlice from "./src/slice/studentSlice";
 import companySlice from "./src/slice/companySlice";
 import interviewSlice from "./src/slice/interviewSlice";
+import interviewResultsSlice from "./src/slice/interviewResultsSlice";
 
 const store = configureStore({
     reducer:{
         auth: authSlice,
         student: studentSlice,
         company: companySlice,
-        interview: interviewSlice
+        interview: interviewSlice,
+        interviewResults: interviewResultsSlice
     }
 });
 
